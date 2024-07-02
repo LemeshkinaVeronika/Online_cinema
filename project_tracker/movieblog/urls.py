7f0945b59e0d2af8495d237f4a17db9c89409ccc
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-07-02 16:21:40 +0300

    Починил поиск

--- project_tracker/movieblog/urls.py
+++ project_tracker/movieblog/urls.py
@@ -12,6 +12,7 @@ sitemaps = {
 
 app_name = 'movieblog'
 urlpatterns = [
+    path('search/', views.SearchPageView.as_view(), name='search_page'),
     path('remove_from_list/', views.RemoveFromListView.as_view(), name='remove_from_list'),
     path('add_to_list/', views.AddToListView.as_view(), name='add_to_list'),
     path("rate_movie/", views.RateMovie, name="rate_movie"),
@@ -19,7 +20,6 @@ urlpatterns = [
     path('category/<int:pk>-<str:slug>/', views.CategoryPageView.as_view(), name='category_page'),
     path('film/<int:pk>-<str:slug>/', views.FilmPageView.as_view(), name='film_page'),
     re_path(r'/(?P<tag_name>[\w-]+)/$', views.TagPageView.as_view(), name='tag_page'),
-    path('search/', views.SearchPageView.as_view(), name='search_page'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path("comment/add/<int:pk>/", views.AddCommentView.as_view(), name="add_comment"),  
     path("comment/edit/<int:pk>/", views.EditCommentView.as_view(), name="edit_comment"),  
